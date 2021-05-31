535d2883aad65ffd67429b420415257df280c7f8
Ori Sayag <sayagori@gmail.com>
2021-05-31 17:01:54 +0300

    shopPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
+
+import Header from "./components/header/header.component";
 import HomePage from "./page/homepage/homepage.component";
+import ShopPage from "./page/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
